d0412e00c8e35cfbcd466b3a4f43d1dd2a6c620a
vitoriaacarvalho <vitoriabcarvalhocorrea@gmail.com>
2025-09-28 14:19:40 -0300

    refactor(backend): add return statements before res.json on questionsController.ts

--- backend/src/controllers/questionsController.ts
+++ backend/src/controllers/questionsController.ts
@@ -4,20 +4,38 @@ import { Difficulty } from "@prisma/client";
 
 export class QuestionsController {
     static async getQuestionsByCategory(req: Request, res: Response) {
-        const { categoryId } = req.params;
-        const questions = await QuestionService.getQuestionsByCategory(categoryId);
-        res.json(questions);
+        try {
+            const { categoryId } = req.params;
+            if(!categoryId) return res.status(400).json({ error: "Error fetching questions by category: category id doesn't exist" });
+
+            const questions = await QuestionService.getQuestionsByCategory(categoryId);
+            return res.json(questions);
+        } catch (error) {
+            return res.status(500).json({ error: "Internal server error" });
+        }
     }
 
     static async getQuestionsByDifficulty(req: Request, res: Response) {
-        const { difficulty } = req.params;
-        const questions = await QuestionService.getQuestionsByDifficulty(difficulty as Difficulty);
-        res.json(questions);
+        try {
+            const { difficulty } = req.params;      
+            if(!difficulty)  
+                return res.status(400).json({ error: "Error fetching questions by difficulty." });
+            const questions = await QuestionService.getQuestionsByDifficulty(difficulty as Difficulty);
+            return res.json(questions);
+        } catch (error) {
+            return res.status(500).json({ error: "Internal server error" });
+        }
     }
 
     static async getQuestionsByCategoryAndDifficulty(req: Request, res: Response) {
-        const { categoryId, difficulty } = req.params;
-        const questions = await QuestionService.getQuestionsByCategoryAndDifficulty(categoryId, difficulty as Difficulty);
-        res.json(questions);
+        try {
+            const { categoryId, difficulty } = req.params;
+            if(!categoryId) return res.status(400).json({ error: "Error fetching questions by category and difficulty: category id doesn't exist" });
+            if(!difficulty) return res.status(400).json({ error: "Error fetching questions by category and difficulty: difficulty doesn't exist" });
+            const questions = await QuestionService.getQuestionsByCategoryAndDifficulty(categoryId, difficulty as Difficulty);
+            return res.json(questions);
+        } catch (error) {
+            return res.status(500).json({ error: "Internal server error" });
+        }
     }
 }   
